7e27962cc1d9cfc228defdc2d98a6cb2062860a6
Sylviachelimo <chelimosylvia280@gmail.com>
2024-04-23 12:44:49 +0300

    Completed

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
@@ -15,9 +16,19 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        """Returns the number of courses the student is enrolled in."""
+        return len(self._enrollments)
+
+    def aggregate_average_grade(self):
+        """Calculates the average grade for the student across all courses."""
+        total_grades = sum(enrollment.grade for enrollment in self._enrollments if enrollment.grade is not None)
+        num_courses = sum(1 for enrollment in self._enrollments if enrollment.grade is not None)
+        average_grade = total_grades / num_courses if num_courses > 0 else 0
+        return average_grade
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -30,15 +41,19 @@ class Course:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def enrollment_count(self):
+        """Returns the total number of enrollments for this course."""
+        return len(self._enrollments)
 
 class Enrollment:
     all = []
-    
-    def __init__(self, student, course):
+
+    def __init__(self, student, course, grade=None):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
             self.course = course
             self._enrollment_date = datetime.now()
+            self.grade = grade
             type(self).all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
